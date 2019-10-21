9efc8274bfe05fa09236a60fdcb6b4a11f988342
Pau Teruel <93.pau.teruel@gmail.com>
2019-10-21 14:32:34 +0200

    setup

---
+++ AptoPCISDK/Classes/PCIView.swift
@@ -0,0 +1,226 @@
+//
+// PCIView.swift
+// AptoPCISDK
+//
+// Created by Ivan Oliver on 15/11/2018.
+//
+
+import UIKit
+import WebKit
+
+public class PCIView: UIView, WKNavigationDelegate, WKUIDelegate, UIWebViewDelegate {
+  let webView = WKWebView(frame: .zero, configuration: WKWebViewConfiguration())
+  var queue = OperationQueue()
+  private let javascriptPrefix = "window.AptoPCISDK"
+  @objc public var styles: [String: Any] = [:] {
+    didSet {
+      customiseUI()
+    }
+  }
+  @objc public var showCvv: Bool = true {
+    didSet {
+      customiseUI()
+    }
+  }
+  @objc public var showExp: Bool = true {
+    didSet {
+      customiseUI()
+    }
+  }
+  @objc public var showPan: Bool = true {
+    didSet {
+      customiseUI()
+    }
+  }
+  @objc public var alertTexts: [String: String]?
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    initQueue()
+    initWebView()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    initQueue()
+    initWebView()
+  }
+  
+  @objc public func initialise(apiKey: String, userToken: String, cardId: String, lastFour: String, environment: String) {
+    queue.addOperation(WebViewOperation(
+      webView: webView,
+      javascript: "\(javascriptPrefix).initialise('\(apiKey)', '\(userToken)', '\(cardId)', '\(lastFour)', '\(environment)')"
+    ))
+  }
+  
+  @objc public func lastFour() {
+    queue.addOperation(WebViewOperation(
+      webView: webView,
+      javascript: "\(javascriptPrefix).lastFour()"
+    ))
+  }
+  
+  @objc public func obfuscate() {
+    queue.addOperation(WebViewOperation(
+      webView: webView,
+      javascript: "\(javascriptPrefix).obfuscate()"
+    ))
+  }
+  
+  @objc public func reveal() {
+    queue.addOperation(WebViewOperation(
+      webView: webView,
+      javascript: "\(javascriptPrefix).reveal()"
+    ))
+  }
+  
+  private func customiseUI() {
+    if let flagsString = dictToString(dict: ["showPan": showPan, "showCvv": showCvv, "showExp": showExp]),
+      let stylesString = dictToString(dict: styles){
+      queue.addOperation(WebViewOperation(
+        webView: webView,
+        javascript: "\(javascriptPrefix).customiseUI('\(flagsString)', '\(stylesString)')"
+      ))
+    }
+  }
+  
+  private func dictToString(dict: [String: Any]) -> String? {
+    if let data = try? JSONSerialization.data(withJSONObject: dict, options: []) {
+      return String(data: data, encoding: .utf8)
+    }
+    return nil
+  }
+
+  private func initQueue() {
+    queue.name = "PCIView operations queue"
+    queue.maxConcurrentOperationCount = 1
+    queue.isSuspended = true
+  }
+  
+  private func initWebView(){
+    webView.translatesAutoresizingMaskIntoConstraints = false
+    addSubview(webView)
+    webView.isOpaque = false
+    webView.backgroundColor = .clear
+    webView.scrollView.backgroundColor = .clear
+    NSLayoutConstraint.activate([
+      webView.topAnchor.constraint(equalTo: self.topAnchor),
+      webView.leftAnchor.constraint(equalTo: self.leftAnchor),
+      webView.rightAnchor.constraint(equalTo: self.rightAnchor),
+      webView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+    ])
+    webView.navigationDelegate = self
+    webView.uiDelegate = self
+    let podBundle = Bundle(for: self.classForCoder)
+    if let myURL = podBundle.url(forResource: "container", withExtension: "html") {
+      webView.loadFileURL(myURL, allowingReadAccessTo: myURL.deletingLastPathComponent())
+    }
+  }
+}
+
+internal class WebViewOperation: Operation {
+  let javascript: String
+  let webView: WKWebView
+  let dispatchQueue: DispatchQueue
+
+  init(webView: WKWebView, javascript: String, on: DispatchQueue? = nil) {
+    self.javascript = javascript
+    self.webView = webView
+    self.dispatchQueue = on ?? DispatchQueue.main
+  }
+
+  override func main() {
+    dispatchQueue.async {
+        self.webView.evaluateJavaScript(self.javascript)
+    }
+  }
+}
+
+public extension PCIView {
+  func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+    queue.isSuspended = false
+  }
+  
+  func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String,
+                      initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
+    let alertController = UIAlertController(title: nil, message: alertText(key: "wrongCode.message"), preferredStyle: .alert)
+    alertController.addAction(UIAlertAction(title: alertText(key: "wrongCode.okAction"), style: .default, handler: { action in
+      completionHandler()
+    }))
+    
+    UIApplication.topViewController()?.present(alertController, animated: true, completion: nil)
+  }
+  
+  func webView(_ webView: WKWebView, runJavaScriptTextInputPanelWithPrompt prompt: String, defaultText: String?,
+                      initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (String?) -> Void) {
+    let alertController = UIAlertController(title: nil, message: alertText(key: "inputCode.message"), preferredStyle: .alert)
+    alertController.addTextField(configurationHandler: { textField in
+      textField.text = defaultText
+    })
+    
+    alertController.addAction(UIAlertAction(title: alertText(key: "inputCode.okAction"), style: .default, handler: { action in
+      if let text = alertController.textFields?.first?.text {
+        completionHandler(text)
+      } else {
+        completionHandler(defaultText)
+      }
+    }))
+    
+    alertController.addAction(UIAlertAction(title: alertText(key: "inputCode.cancelAction"), style: .default, handler: { action in
+      completionHandler(nil)
+    }))
+    
+    UIApplication.topViewController()?.present(alertController, animated: true, completion: nil)
+  }
+  
+  func alertText(key: String) -> String {
+    if let text = alertTexts?[key] {
+      return text
+    } else {
+      return key.podLocalized()
+    }
+  }
+}
+
+public extension String {
+  func podLocalized() -> String {
+    return self.podLocalized(PCIView.classForCoder())
+  }
+  
+  func podLocalized(_ bundleClass:AnyClass) -> String {
+    return getBundleTranslation(Bundle(for:bundleClass))
+  }
+  
+  fileprivate func getBundleTranslation(_ bundle:Bundle) -> String {
+    var language =  Locale.preferredLanguages[0].lowercased()
+    let path = bundle.path(forResource: language, ofType: "lproj")
+    if let path = path, let languageBundle = Bundle(path: path) {
+      return NSLocalizedString(self, bundle: languageBundle, comment: "")
+    }
+    else {
+      // Try with just the language without region
+      language = language.prefixUntil("-")
+      let path = bundle.path(forResource: language, ofType: "lproj")
+      if let path = path, let languageBundle = Bundle(path: path) {
+        return NSLocalizedString(self, bundle: languageBundle, comment: "")
+      }
+      else {
+        // Fall back to the user's language
+        return NSLocalizedString(self, bundle: bundle, comment: "")
+      }
+    }
+  }
+  
+  fileprivate func prefixUntil(_ string:String) -> String {
+    if let range = self.range(of: string) {
+      let intIndex: Int = self.distance(from: self.startIndex, to: range.lowerBound)
+      return self.prefixOf(intIndex)!
+    }
+    return self
+  }
+  
+  fileprivate func prefixOf(_ size:Int) -> String? {
+    return String(self.prefix(size))
+  }
+}
+
---
+++ README.md
@@ -0,0 +1,105 @@
+# AptoPCISDK
+
+[![CI Status](https://img.shields.io/travis/ioliver/AptoPCISDK.svg?style=flat)](https://travis-ci.org/ioliver/AptoPCISDK)
+[![Version](https://img.shields.io/cocoapods/v/AptoPCISDK.svg?style=flat)](https://cocoapods.org/pods/AptoPCISDK)
+[![License](https://img.shields.io/cocoapods/l/AptoPCISDK.svg?style=flat)](https://cocoapods.org/pods/AptoPCISDK)
+[![Platform](https://img.shields.io/cocoapods/p/AptoPCISDK.svg?style=flat)](https://cocoapods.org/pods/AptoPCISDK)
+
+## Example
+
+To run the example project, clone the repo, and run `pod install` from the Example directory first.
+
+## Requirements
+
+## Installation
+
+AptoPCISDK is available through [CocoaPods](https://cocoapods.org). To install
+it, simply add the following line to your Podfile:
+
+```ruby
+pod 'AptoPCISDK'
+```
+
+## Using the PCI SDK
+
+To use the PCI SDK, simply instantiate a PCIView and add it to yor view hierarchy:
+
+```swift
+view.addSubview(pciView)
+pciView.translatesAutoresizingMaskIntoConstraints = false
+NSLayoutConstraint.activate([
+  pciView.topAnchor.constraint(equalTo: topConstraint, constant: 40),
+  pciView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+  pciView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20),
+  pciView.heightAnchor.constraint(equalToConstant: 240),
+])
+```
+
+Then, initialize the SDK using the initialize medhod:
+
+```swift
+pciView.initialise(apiKey: "API_KEY",
+                   userToken: "USER_TOKEN",
+                   cardId: "CARD_ID",
+                   lastFour: "1234",
+                   environment: "sandbox")
+```
+
+Note: The allowed values for the `environment` parameter are `sandbox` and `production`.
+
+To show the card's last four, use the following snippet:
+
+```swift
+pciView.lastFour()
+```
+
+To show the card's complete data, use the following snippet:
+
+```swift
+pciView.reveal()
+```
+
+The PCI SDK will verify the user (if needed) and will show the card data.
+
+## Customise the PCI SDK
+
+The PCI SDK look & feel can be customised in different ways:
+
+### Showing / hiding elements
+
+You can decide which elements are shown by the PCI SDK. There are three elements that can be shown / hidden:
+
+1. PAN
+1. CVV
+1. Exp. Date
+
+You can, also, define the css styles of various components, including:
+
+1. IFrame (container)
+1. PAN field.
+1. CVV field.
+1. Exp. Date field
+
+To customise the PCI look and feel, you can use the follwing snippet:
+
+```swift
+pciView.showPan = true
+pciView.showCvv = false
+pciView.showExp = false
+pciView.styles = [
+  "container": "IFrame CSS Styles",
+  "content": [
+    "pan": "PAN CSS Styles",
+    "cvv": "CVV CSS Styles",
+    "exp": "Exp. Date CSS Styles"
+  ]
+]
+```
+
+## Author
+
+Pau Teruel, pau@aptopayments.com
+
+## License
+
+AptoPCISDK is available under the MIT license. See the LICENSE file for more info.
